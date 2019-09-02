7d5f58d899649bb741edc886583aa6b928e3ed4e
wsx <1459775922@qq.com>
2019-09-02 14:49:55 +0800

    post请求

--- controllers/user.js
+++ controllers/user.js
@@ -19,6 +19,79 @@ let getUsers = async (ctx, next) => {
     };
 };
 
-module.exports = {
-    'GET /user/getUsers': getUsers
+let login = async (ctx, next) => {
+    ctx.response.type = 'application/json';
+    console.log(JSON.stringify(ctx));
+    let userS = await Pet.findAll({
+        // where: {
+        //     name: para.query.name
+        // }
+    });
+    // 设置Response Body:
+    ctx.response.body = {
+        data: userS
+    };
 };
+
+module.exports = {
+    'GET /user/getUsers': getUsers,
+    'POST /user/login': login,
+};
+
+
+//-----------------------------数据库操作----start
+//便利数据库表并初始化表数据
+// const model = require('./model');
+//
+// let Pet = model.Pet;
+
+//添加数据
+// (async () => {
+//     let now = Date.now();
+//     let dog = await Pet.create({
+//         id: 'd-' + now,
+//         name: 'test22',
+//         gender: false,
+//         birth: '2008-08-08',
+//         createdAt: now,
+//         updatedAt: now,
+//         version: 0
+//     });
+//     console.log('created: ' + JSON.stringify(dog));
+// })();
+
+//删除数据
+// (async () => {
+//     var p = await queryFromSomewhere();
+//     await p.destroy();
+// })();
+
+//更新数据
+// (async () => {
+//     let pets = await Pet.findAll({
+//         where: {
+//             version: '0'
+//         }
+//     });
+//     for (let p of pets) {
+//         p.gender = true;
+//         p.updatedAt = Date.now();
+//         p.version++;
+//         await p.save();
+//     }
+// })();
+
+//查询数据
+// (async () => {
+//     let pets = await Pet.findAll({
+//         where: {
+//             name: 'Gaffey'
+//         }
+//     });
+//     console.log(`find ${pets.length} pets:`);
+//     for (let p of pets) {
+//         console.log(JSON.stringify(p));
+//     }
+// })();
+
+//-----------------------------数据库操作----end
--- models/Pet.js
+++ models/Pet.js
@@ -3,5 +3,6 @@ const db = require('../db');
 module.exports = db.defineModel('pets', {
     birth: db.Sequelize.STRING(100),
     name: db.Sequelize.STRING(10),
-    gender: db.Sequelize.BOOLEAN
+    gender: db.Sequelize.BOOLEAN,
+    age:db.Sequelize.INTEGER,
 });
